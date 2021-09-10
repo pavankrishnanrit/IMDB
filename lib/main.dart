b5c83f15b4f8ccf9e9d70748efabc5ea85001751
pavankrishnanrit <pavankrishnanr.it2020@citchennai.net>
2021-09-10 22:21:59 +0530

    'changes'

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main()=>runApp(new MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark,
+      primaryColor: Colors.blue[900]),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('IMDB'),)
+    );
+  }
+}
